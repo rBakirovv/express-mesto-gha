2f4fbb9779fe0bf89814b46e08a21f4f7ad8292b
Руслан Бакиров <ruslanbakirov@MacBook-Air-Ruslan.local>
2022-03-07 23:48:34 +0300

    first commit

---
+++ models/card.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const user = require('./user');
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.ObjectId,
+    required: true,
+    ref: user,
+  },
+  likes: [{
+    type: mongoose.ObjectId,
+    default: [],
+    ref: user,
+  }],
+  createdAt: [{
+    type: Date,
+    default: Date.now(),
+    ref: user,
+  }],
+});
+
+module.exports = mongoose.model('card', cardSchema);
